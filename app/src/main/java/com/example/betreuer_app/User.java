e61a57e435780f36f786c66dda221cc6ab3a2d0a
StefanSchaerl <stefan.schaerl@iu-study.org>
2025-12-05 10:57:39 +0100

    feat: Add User entitiy - Stefans Task

---
+++ app/src/main/java/com/example/betreuer_app/User.java
@@ -0,0 +1,59 @@
+package com.example.betreuer_app;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String passwordHash;
+
+    public User(UUID id, String firstName, String lastName, String email, String passwordHash) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+}
