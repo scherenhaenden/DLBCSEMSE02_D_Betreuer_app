cc522a8e316b399997b97e402d6fc6c54720b901
Abraham Angene <124372191+abe-maker@users.noreply.github.com>
2025-12-11 17:58:27 +0100

    Klasse, Attribut und Getter und Setter erstellt

---
+++ app/src/main/java/com/example/betreuer_app/model/Topic.java
@@ -0,0 +1,88 @@
+package com.example.betreuer_app.model;
+
+public class Topic {
+    
+    private int id;
+    private String title;
+    private String description;
+    private String subjectArea;
+    private boolean isActive;
+
+    public topic(int id, String title, String description, String subjectArea, boolean isActive) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.subjectArea = subjectArea;
+        this.isActive = isActive;
+    }
+
+    /*
+    Returns the ID.
+    */
+    public int getID() {
+        return id;
+    }
+
+    /*
+    Sets the ID.
+    */
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    /*
+    Returns the title.
+    */
+    public String getTitle() {
+        return title;
+    }
+
+    /*
+    Sets the title
+    */
+    public void setTitle(String title) {
+        this.title = tile;
+    }
+
+    /*
+    Returns the description.
+    */
+    public String getDescription() {
+        return description;
+    }
+
+    /*
+    Set the description.
+    */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /*
+    Returns the subjectArea.
+    */
+    public String getSubjectArea() {
+        return subjectArea;
+    }
+
+    /*
+    Sets the subjectArea.
+    */
+    public void setSubjectAre(String subjectArea) {
+        this.subjectArea = subjectArea;
+    }
+
+    /*
+    Returns the isActive.
+    */
+    public boolean getIsActive() {
+        return isActive;
+    }
+
+    /*
+    Sets isActive.
+    */
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+}
