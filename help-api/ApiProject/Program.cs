a9391e6ff8d018e92dbfd37fe9b119b256e5dbea
scherenhaenden <scherenhaenden@gmail.com>
2025-12-14 12:37:53 +0100

    Refactor code structure for improved readability and maintainability

--- help-api/ApiProject/Program.cs
+++ help-api/ApiProject/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls(builder.Configuration["Urls"]);
 
 // Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.ConfigureSwagger();
+builder.Services.AddEndpointsApiExplorer(); // Required for Swashbuckle
+builder.Services.ConfigureSwagger(); // Use your extension
 builder.Services.AddControllers();
 builder.Services.ConfigureJwtAuthentication(builder.Configuration);
 builder.Services.ConfigureDatabase(builder.Configuration);
@@ -20,19 +20,12 @@ builder.Services.AddScoped<SeedService>();
 var app = builder.Build();
 string directory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", ".."));
 
-
-
 // Seed the database
 using (var scope = app.Services.CreateScope())
 {
     var seedService = scope.ServiceProvider.GetRequiredService<SeedService>();
     await seedService.SeedAsync();
 }
-
-// get current directory
-
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,7 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Thesis Management API v1");
-        c.InjectStylesheet("/swagger-dark.css"); // Add this line
+        c.InjectStylesheet("/swagger-dark.css");
     });
 }
 
