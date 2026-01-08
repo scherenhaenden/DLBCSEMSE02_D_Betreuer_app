680e96d9d6e3898ff6ff941c5ce0f14fb7d4bbca
scherenhaenden <scherenhaenden@gmail.com>
2026-01-08 19:48:41 +0100

    feat: Add BillingStatusResponse model to represent billing status data

---
+++ help-api/ApiProject/ApiLogic/Models/BillingStatusResponse.cs
@@ -0,0 +1,9 @@
+namespace ApiProject.ApiLogic.Models
+{
+    public class BillingStatusResponse
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
+
